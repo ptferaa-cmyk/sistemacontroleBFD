26f33603a60de19ea4a6a37fac758c1e0add4bee
Vivian RC <squeenswan@gmail.com>
2026-08-15 19:06:59 -0300

    Remove assets e cidade-da-arte

--- cidade-da-arte/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://jzkfpxoibclszoxyffog.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_6nDfXRIN7dPnrjFGtb5JEQ_RK7Wk4bh";
-
-const clienteSupabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
